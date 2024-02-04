51e4166410e9d71467a437b01ab1be8d56976061
Salsedini <d.s.022222@gmail.com>
2024-02-04 21:01:07 +0100

    Docker added

--- fastapi/app/config.py
+++ fastapi/app/config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://Salcedo:1234@localhost/dbname"
+DATABASE_URL = "postgresql://Salcedo:1234@db/dbname"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autflush = False, bind = engine)
